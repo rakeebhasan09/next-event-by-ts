3b2f07d76eb756434e730f0a63d1da1b6f75021a
rakeebhasan <mdrakeebhasan1@gmail.com>
2026-03-07 16:27:07 +0600

    Start learn typescript.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
-		<div className="capitalize text-center text-5xl font-medium">
-			hello world
+		<div>
+			<h2 className="capitalize text-center text-5xl font-medium">
+				hello world
+			</h2>
+			<Button>Click Me</Button>
 		</div>
 	);
 }
